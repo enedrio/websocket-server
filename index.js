9f1b2e5892bf5f6a8e188d19d628009ac0c9abdc
Eric Busch <busch@jymmin.com>
2021-08-13 11:55:21 +0200

    implement matrix broadcast

--- index.js
+++ index.js
@@ -5,20 +5,58 @@ const wss = new ws.Server({
   perMessageDeflate: false
 });
 
+const state = {
+    matrix: []
+}
+
+for (let i=0; i < 64; i++) {
+    state.matrix.push(0);
+}
+
+function sendMatrix(client) {
+    if (client.readyState === 1) {
+        client.send(JSON.stringify({
+            addr: '/matrix',
+            matrix: state.matrix
+        }));
+    }
+}
+
+
 const dispatcher = {
-    '/matrix': (m) => { console.log('do matrix stuff') },
+    '/matrix': (m) => {
+        console.log('matrix has been updated, broadcast changes to all clients')
+        state.matrix = m.matrix
+        wss.clients.forEach(function each(client) {    
+            sendMatrix(client)
+        });
+    },
+    '/get-matrix': (_, client) => { 
+        console.log('send matrix to client')
+        client.send(JSON.stringify({
+            addr: '/matrix',
+            matrix: state.matrix
+        }))
+    }
 }
 
 const clients = {}
 
 
 wss.on('connection', function connection(ws, _, client) {
+    console.log('client connected')
+    ws.isAlive = true
     ws.on('message', function incoming(message) {
         console.log('received: %s', message);
-        const m = JSON.parse(message)
-        if (dispatcher[m.addr] !== undefined) dispatcher[m.addr](m)
+        try {
+            const m = JSON.parse(message)
+            if (dispatcher[m.addr] !== undefined) dispatcher[m.addr](m, ws)
+        } catch (e) {
+            console.warn('Not a JSON message');
+            console.log(e)
+        }
     });
 
-    ws.send('something');
+    sendMatrix(ws)
 });
 
